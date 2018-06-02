95663009ae6316fdfa6a7616af9fd4834e312f6a
helianyang <471177435@qq.com>
2018-06-02 18:56:28 +0800

    增加搜索功能

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@
 // let len = 6;
 !function init() {
   opData()
-  count()
-  // roll() 
+  count(data)
+  search()
 }()
 
 // setInterval(function () {
 //   opData()
 // }, 2000);
 function opData() {
+  let ul = document.getElementsByClassName('content-list');
   // let ul = document.getElementsByClassName('content-list');
   // if (index > data.length) {
   //   index = 0;
@@ -20,63 +21,35 @@ function opData() {
   // len = len + 6;
   // ul[0].innerHTML = '';
   data.forEach((ele, index)=> {
-    createList(ele, index)
+    createList(ele, index, ul[0]);
   })
 }
-function count() {
+function count(arr) {
   let span = document.getElementsByClassName('count')[0].children[0];
-  let len = data.length;
-  span.innerHTML = `一共 ${len} 条心愿`;
-  console.log(span);
+  let len = arr.length;
+  span.innerHTML = `共 ${len} 条心愿`;
 }
 
-function createList(ele, index) {
+function createList(ele, index, dom) {
   
-  let ul = document.getElementsByClassName('content-list');
+  
   let li = document.createElement('li');
-  // index = index * 2;
   li.innerHTML = ele;
  
   if (index % 2) {
     li.style.backgroundColor = '#f31055';
     li.style.color = '#fff';
   }
-  // if (index/6 === 1) {
-  //   li.style.backgroundColor = '#3f3f3f';
-  //   li.style.color = '#fff';
-  // }
   
-  ul[0].appendChild(li);
+  dom.appendChild(li);
   
 }
 
-// function roll() {
-//   var area =document.getElementsByClassName('content')[0];
-//   var con1 = document.getElementsByClassName('content-list')[0];
-//   var con2 = document.getElementsByClassName('content-list1')[0];
-//   con2.innerHTML=con1.innerHTML;
-//   function scrollUp(){
-//   if(area.scrollTop>=con1.offsetHeight){
-//       area.scrollTop=0;
-//   }else{
-//       area.scrollTop = area.scrollTop + 1;
-//   }
-//   }                
-//   var time = 10;
-//   var mytimer=setInterval(scrollUp,time);
-//   // area.onmouseover=function(){
-//   //     clearInterval(mytimer);
-//   // }
-//   // area.onmouseout=function(){
-//   //     mytimer=setInterval(scrollUp,time);
-//   // }
-// }
 
 setTimeout(change,3000); 
 	function upscroll(){
 		var content = $(".commentBox-content");
     var offset = ($(".commentBox-content").find("li").eq(0).height()+35)*-1 + "px";
-    console.log(offset)
 		content.stop().animate({top:offset},2000,function(){
 			var first = $("li").first();
 			$(".commentBox-content").append(first);
@@ -85,7 +58,29 @@ setTimeout(change,3000);
 		
 		setTimeout(change,3000);
 	}
-	
-	function change(){
-		upscroll();
-	}
+
+function change() {
+  upscroll();
+}
+
+function search() {
+  let search = document.getElementsByClassName('in-edit')[0];
+  let btn = document.getElementsByClassName('btn')[0];
+  let list = document.getElementsByClassName('content-list');
+  btn.addEventListener('click', function () {
+    if (!search.value) {
+      return;
+    }
+    list[1].innerHTML = '';
+    list[0].style.display = 'none';
+    list[1].style.display = 'block';
+    let word = search.value;
+    let result = data.filter( ele => ele.indexOf(word) !== -1);
+    count(result);
+    result.forEach((ele, index) => {
+      createList(ele, index, list[1]);
+    })
+    
+    search.value = '';
+  }, false);
+}
